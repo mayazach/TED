5177bca8b3ae01bf84e723722bd7ad75021f2a37
mayazaha <mayazaha2@gmail.com>
2017-08-28 23:02:47 +0300

    Minor bug fixes

--- src/main/java/com/project/rooms/controllers/SearchController.java
+++ src/main/java/com/project/rooms/controllers/SearchController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.project.rooms.dao.RoomDAO;
 import com.project.rooms.entities.Room;
--- src/main/resources/static/dokimi.jsp
+++ src/main/resources/static/dokimi.jsp
@@ -1,7 +1,6 @@
 <html>
-<head><title>JSP page</title></head>>
+<head><title>JSP page</title></head>
 <body>
 <%out.println("Hello World"); %>
-</body>>
-</title>
+</body>
 </html>
