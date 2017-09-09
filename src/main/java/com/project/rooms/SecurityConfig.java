ed65efaa0d2a4b5bfa3d69bbf661f3aff70a7abe
mayazaha <mayazaha2@gmail.com>
2017-09-09 21:55:47 +0300

    Role-specific pages can only be visited by user with the right role

--- src/main/java/com/project/rooms/SecurityConfig.java
+++ src/main/java/com/project/rooms/SecurityConfig.java
@@ -23,7 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/search").permitAll()
+                .antMatchers("/", "/search","/showroom/**").permitAll()
+                .antMatchers("/adminpanel").hasAuthority("admin")
+                .antMatchers("/managerooms").hasAuthority("owner")
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -39,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/fonts/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/fonts/**","/WEB-INF**");
     }
 	
 	@Override
