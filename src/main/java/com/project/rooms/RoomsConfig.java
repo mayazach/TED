a895e8b3aa1b3c9a7bec5c31613c7aeb242efb4d
mayazaha <mayazaha2@gmail.com>
2017-09-09 12:13:46 +0300

    Users authenticated from database

--- src/main/java/com/project/rooms/RoomsConfig.java
+++ src/main/java/com/project/rooms/RoomsConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -48,5 +49,6 @@ public class RoomsConfig {
             @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
     }
+
 
 }
--- src/main/java/com/project/rooms/dao/UserDAO.java
+++ src/main/java/com/project/rooms/dao/UserDAO.java
@@ -1,8 +1,14 @@
 package com.project.rooms.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.project.rooms.entities.User;
 
 public interface UserDAO extends CrudRepository<User, Long>{
+	
+	@Query("SELECT u FROM User u WHERE u.username = :username")
+	User findUserByUsername(@Param("username") String username);
 
 }
